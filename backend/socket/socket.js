21d2131c4d2013f4a6eae84de127650b232ba05e
PurvaPotbhare <purvapotbhare4@gmail.com>
2026-02-15 12:46:35 +0530

    Fix socket poll room join

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,7 +1,14 @@
 module.exports = (io) => {
   io.on("connection", (socket) => {
+    console.log("🟢 User connected:", socket.id);
+
     socket.on("joinPoll", (pollId) => {
       socket.join(pollId);
+      console.log(`📊 Joined poll room: ${pollId}`);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("🔴 User disconnected:", socket.id);
     });
   });
 };
